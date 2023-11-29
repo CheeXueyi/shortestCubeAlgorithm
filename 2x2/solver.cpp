d3585bb8bb76b1bfc6eea36bbb4876272cf970b0
Xueyi <z5483716@ad.unsw.edu.au>
2023-11-30 05:17:04 +1100

    starting on 2x2 solver

---
+++ 2x2/solver.cpp
@@ -0,0 +1,15 @@
+enum colours { white, orange, green, red, blue, yellow };
+
+class Cube {
+    // properties
+    colours faces[6][4];
+
+    /**
+     * solves the cube and returns the algorithm
+    */
+    int solve(char [20]) {
+        return false;
+    }
+
+        
+};
